a084932dad9c013c210015ea589c1b29295e3938
Antonio Sousa <developerantoniosousa@gmail.com>
2026-05-07 17:26:11 -0300

    feat: add colors theme

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { colors } from "@/theme/colors";
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: colors.white,
     alignItems: "center",
     justifyContent: "center",
   },
