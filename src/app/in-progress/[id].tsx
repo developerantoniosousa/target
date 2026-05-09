144957c35412f8960b1b3d78eb46ea4f53ddafad
Antonio Sousa <developerantoniosousa@gmail.com>
2026-05-09 10:56:42 -0300

    feat: build Progress

---
+++ src/app/in-progress/[id].tsx
@@ -0,0 +1,23 @@
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+import { useLocalSearchParams } from "expo-router";
+import { View } from "react-native";
+
+const details = { current: "R$ 580,00", percentage: 25, target: "R$ 1.790,00" };
+
+export default function InProgress() {
+  const params = useLocalSearchParams<{ id: string }>();
+
+  return (
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch"
+        rightButton={{
+          icon: "edit",
+          onPress: () => {},
+        }}
+      />
+      <Progress data={details} />
+    </View>
+  );
+}
