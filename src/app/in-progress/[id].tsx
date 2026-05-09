9528aec2ac0f10fcdfd1a1f348612234693a9cc2
Antonio Sousa <developerantoniosousa@gmail.com>
2026-05-09 11:17:05 -0300

    feat: build Transaction

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,9 +1,35 @@
-import { PageHeader } from "@/components/PageHeader";
-import { Progress } from "@/components/Progress";
 import { useLocalSearchParams } from "expo-router";
 import { View } from "react-native";
 
+import { List } from "@/components/List";
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+import { Transaction, TransactionProps } from "@/components/Transaction";
+import { TransactionTypes } from "@/utils/TransactionTypes";
+
 const details = { current: "R$ 580,00", percentage: 25, target: "R$ 1.790,00" };
+const transactions: TransactionProps[] = [
+  {
+    id: "1",
+    date: "12/04/25",
+    type: TransactionTypes.Output,
+    value: "R$ 20,00",
+  },
+  {
+    id: "2",
+    date: "12/04/25",
+    type: TransactionTypes.Input,
+    value: "R$ 300,00",
+    description: "CDB de 110% no banco XPTO",
+  },
+  {
+    id: "3",
+    date: "12/04/25",
+    type: TransactionTypes.Input,
+    value: "R$ 300,00",
+    description: "CDB de 110% no banco XPTO",
+  },
+];
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
@@ -18,6 +44,13 @@ export default function InProgress() {
         }}
       />
       <Progress data={details} />
+      <List
+        title="Transações"
+        data={transactions}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+      />
     </View>
   );
 }
